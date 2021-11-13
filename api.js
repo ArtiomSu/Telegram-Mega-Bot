6f05b8ff6a61710ed4c498899aca12cf75c51789
ArtiomSu <artiom.sumigora@mycit.ie>
2021-11-13 22:12:13 +0000

    add options header to api

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
